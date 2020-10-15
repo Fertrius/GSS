6e0ed798f9c2194050c0c5486c221818cbe7d34f
Fertrius <aasavchuk@gmail.com>
2020-10-15 16:14:28 +0300

    Update gss.ino

--- gss.ino
+++ gss.ino
@@ -1,3 +1,17 @@
+// Explanation:
+// outside - city power line
+// inside - generator power line
+// local -
+
+// airpumpon - 
+// airpumpoff - 
+
+
+const int outside = 
+const int local = 
+const int inside =
+
+
 void setup() {
   // put your setup code here, to run once:
 
